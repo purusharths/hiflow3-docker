814325eb6e49460501c16a68a6682c30930dc025
Purusharth Saxena <purusharth.sa@gmail.com>
2022-01-31 01:47:03 +0100

    intial commit

---
+++ download_hiflow.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+mkdir -p hiflow3-v3_teaching
+cd hiflow3-v3_teaching
+wget --no-check-certificate https://emcl-gitlab.iwr.uni-heidelberg.de/hiflow3.org/hiflow3/-/archive/v3_teaching/hiflow3-v3_teaching.tar.gz -O - | tar -xz
+sudo docker-compose run hiflow3 /bin/bash -c "mkdir build; cd build;
+cmake .. -DHIFLOW_CPP_ISO_STANDARD=c++17;
+make -j$(nproc)"
+
