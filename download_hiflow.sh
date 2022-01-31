56016620ebbe82b556e43b691d6160321540cda7
Purusharth Saxena <purusharth.sa@gmail.com>
2022-01-31 16:26:38 +0100

    generic folder name

--- download_hiflow.sh
+++ download_hiflow.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-mkdir -p hiflow3-v3_teaching
-cd hiflow3-v3_teaching
+mkdir -p hiflow
+cd hiflow
 wget --no-check-certificate https://emcl-gitlab.iwr.uni-heidelberg.de/hiflow3.org/hiflow3/-/archive/v3_teaching/hiflow3-v3_teaching.tar.gz -O - | tar -xz
 sudo docker-compose run hiflow3 /bin/bash -c "mkdir build; cd build;
 cmake .. -DHIFLOW_CPP_ISO_STANDARD=c++17;
